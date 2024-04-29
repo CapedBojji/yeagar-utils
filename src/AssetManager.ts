d941103e848b5b1f7008697a25dbe0ebaaced066
Laud Boateng <capedbojji@gmail.com>
2024-04-29 07:40:08 +0000

    Apr 29, 2024, 3:40 AM

--- src/AssetManager.ts
+++ src/AssetManager.ts
@@ -1,4 +1,30 @@
+/// <reference types="@rbxts/types" />
+import { InstanceTree } from "@rbxts/validate-tree";
+import { InstanceCache } from "@rbxts/instance-cache";
+import { Workspace } from "@rbxts/services";
+
+const CF_REALLY_FAR_AWAY = new CFrame(0, 10e4, 0);
 
 export class AssetManager {
+	private readonly assets: Map<string, InstanceCache<Instance>> = new Map();
+	private readonly baseparts: Map<string, InstanceCache<BasePart>> = new Map();
+	private readonly assetPositions: Map<string, Vector3> = new Map();
+
+	constructor(private readonly assetRoot: Instance) {}
+
+	cacheAssetBasepart(assetName: string, asset: BasePart, tree: InstanceTree, location?: Instance, amount?: number) {
+		if (this.baseparts.has(assetName)) return warn(`Basepart ${assetName} already cached`);
+		asset.CFrame = CF_REALLY_FAR_AWAY;
+		const cache = new InstanceCache(asset, tree, location, amount);
+		this.baseparts.set(assetName, cache);
+	}
+
+	returnAssetBasepart(assetName: string, asset: BasePart) {
+		const cache = this.baseparts.get(assetName);
+		if (!cache) return warn(`Basepart ${assetName} not cached`);
+		asset.CFrame = CF_REALLY_FAR_AWAY;
+		cache.Return(asset);
+	}
+
     
 }
